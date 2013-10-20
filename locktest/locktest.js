6672817f4d4069ea493a216258f7da90338f8ae7
Chris Brown <chris@thebrown.net>
2013-10-20 20:59:25 +0100

    Tidy up, renamed some functions

--- locktest/locktest.js
+++ locktest/locktest.js
@@ -31,17 +31,16 @@ Q.longStackSupport = true;
 var ROLLBACK = 'ROLLBACK';
 var STATUS = 'SHOW ENGINE INNODB STATUS';
 
-var delay = 50;
-
 var statements = [
     'DELETE FROM test',
-    'DELETE FROM test where a < 5'
+    'DELETE FROM test WHERE a < 5',
+    'DELETE FROM test WHERE a = 2'
 ];
 
-run_tests(statements, delay);
+runTests(statements, 50);
 
 
-function run_tests(statements, delay) {
+function runTests(statements, delay) {
     var con1 = connect();
     var con2 = connect();
 
@@ -52,19 +51,19 @@ function run_tests(statements, delay) {
     for (var i = 0; i < statements.length; i++) {
         var statement = statements[i];
 
-        promise = promise.then(function() {
-            return run_test(con1, con2, statement, statement, delay);
-        }).then(log_status);
+        promise = promise.then(function runOneTest() {
+            return runTest(con1, con2, statement, statement, delay);
+        }).then(logStatus);
     }
 
-    promise.done(function() {
+    promise.done(function tearDown() {
         disableLockMonitor(con1);
         con1.end();
         con2.end();
     });
 }
 
-function log_status(status) {
+function logStatus(status) {
     var matches = status.match(/^.*lock.*$/mg);
     for (var i = 0; i < matches.length; i++) {
         console.log(matches[i]);
@@ -73,9 +72,9 @@ function log_status(status) {
 
 // Returns a function which executes the given SQL statement and returns a promise of the result.
 // The function expects to be passed the result of a previous promise (which it ignores) and a callback to invoke when the SQL has been executed.
-function bind_query(con, sql) {
+function bindQuery(con, sql) {
     // Ignore the result from the previous chained promise
-    return Q.nbind(function(ignored_result, cb) {
+    return Q.nbind(function(ignoredResult, cb) {
         con.query(sql, cb);
     });
 }
@@ -83,11 +82,11 @@ function bind_query(con, sql) {
 // Given a function, fn, which returns a promise, return a function which calls fn but ignores its result.
 // The function instead chains the previous promise.
 function ignore(fn, thisArg) {
-    return function(outer_result) {
+    return function(outerResult) {
         var deferred = Q.defer();
-        fn.call(thisArg, outer_result)
-            .done(function onFulfilled(ignored_result) {
-                deferred.resolve(outer_result);
+        fn.call(thisArg, outerResult)
+            .done(function onFulfilled(ignoredResult) {
+                deferred.resolve(outerResult);
             }, function onRejected(error) {
                 deferred.reject(error);
             });
@@ -95,19 +94,19 @@ function ignore(fn, thisArg) {
     }
 }
 
-function run_test(con1, con2, sql1, sql2, delay) {
-    return bind_query(con1, sql1)(null)
-        .then(function do_all() {
+function runTest(con1, con2, sql1, sql2, delay) {
+    return bindQuery(con1, sql1)(null)
+        .then(function doAll() {
             return Q.all([
-                bind_query(con2, sql2)(null)
-                    .then(bind_query(con2, ROLLBACK)),
+                bindQuery(con2, sql2)(null)
+                    .then(bindQuery(con2, ROLLBACK)),
 
                 Q.delay(delay)
-                    .then(bind_query(con1, STATUS))
-                    .then(ignore(bind_query(con1, ROLLBACK)))
+                    .then(bindQuery(con1, STATUS))
+                    .then(ignore(bindQuery(con1, ROLLBACK)))
             ]);
         })
-        .spread(function show_result(result1, result2) {
+        .spread(function extractStatus(result1, result2) {
             return Q(result2[0][0].Status);
         });
 }
